3af890ffe5e142461f341d65ae7dd8f9004b6fae
Aseer KT <aseermiqdad@gmail.com>
2021-03-07 13:09:28 +0530

    Removed dist folder which contains compiled js files

--- server/dist/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.COOKIE_NAME = exports.PORT = void 0;
-exports.PORT = process.env.PORT || 5000;
-exports.COOKIE_NAME = 'gitToken';
-//# sourceMappingURL=constants.js.map
