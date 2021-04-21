9d25ed04a0887b9e60003347ce560607ff04fc03
Aseer KT <aseermiqdad@gmail.com>
2021-04-21 20:06:20 +0530

    remove auth header in in withApollo

--- client/src/utils/withApollo.ts
+++ client/src/utils/withApollo.ts
@@ -1,20 +1,13 @@
 import { createWithApollo } from './createWithApollo';
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { NextPageContext } from 'next';
-import { getTokenFromRequest } from './getTokenFromRequest';
 import { createUploadLink } from 'apollo-upload-client';
 import { API_URL } from '../config';
 
-const createClient = (ctx: NextPageContext) => {
-  console.log({ ctx });
-  const jwt = getTokenFromRequest(ctx);
-
+const createClient = (_ctx: NextPageContext) => {
   const uploadLink = createUploadLink({
     uri: `${API_URL}/graphql`,
     credentials: 'include',
-    headers: {
-      authorization: `Bearer ${jwt}`,
-    },
   });
 
   return new ApolloClient({
